754d086b3182b435f0572c0e63d8272e4a945e2b
Pablo-Carreras <pablo.carreras-canton@mths.ca>
2024-10-24 10:58:36 -0400

    did the code

--- main.ts
+++ main.ts
@@ -1,8 +1,46 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created by: Pablo Carreras
+ * Created on: Oct 2024
  * This program ...
 */
 
-basic.showString('Hello, World!')
+//variable 
+let disctanceToObject: number = 0
+let neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+
+//setup 
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+basic.showIcon(IconNames.Happy)
+
+//find the disctance of the sonar 
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    disctanceToObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+    if (disctanceToObject < 10){
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.show()
+    }
+    else {
+        disctanceToObject >= 10 
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.show()
+    }
+    
+})
+
